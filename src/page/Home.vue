9e44f6345680353d3df2b7b1265ae73ae96e5126
21克的爱情 <raohong07@163.com>
2018-08-27 09:22:50 +0800

    简单登录制作

---
+++ src/page/Home.vue
@@ -0,0 +1,71 @@
+<template>
+  <div>
+    <group>
+      <cell is-link title="Simple" link="/login"></cell>
+      <cell is-link title="Switch icons" link="/component/tabbar-icon"></cell>
+    </group>
+    <router-view></router-view>
+    <tabbar>
+      <tabbar-item>
+        <img slot="icon" src="../assets/tab/icon_nav_button.png">
+        <span slot="label">Wechat</span>
+      </tabbar-item>
+      <tabbar-item show-dot>
+        <img slot="icon" src="../assets/tab/icon_nav_msg.png">
+        <span slot="label">Message</span>
+      </tabbar-item>
+      <tabbar-item selected link="/component/demo">
+        <img slot="icon" src="../assets/tab/icon_nav_article.png">
+        <span slot="label">Explore</span>
+      </tabbar-item>
+      <tabbar-item badge="2">
+        <img slot="icon" src="../assets/tab/icon_nav_cell.png">
+        <span slot="label">News</span>
+      </tabbar-item>
+    </tabbar>
+  </div>
+</template>
+
+<script>
+import { mapActions } from 'vuex'
+import { Tabbar, TabbarItem, Group, Cell } from 'vux'
+import { loginByUsername } from '@/utils/api'
+
+export default {
+  components: {
+    Tabbar,
+    TabbarItem,
+    Group,
+    Cell
+  },
+  data () {
+    return {
+      msg: 'Hello World!'
+    }
+  },
+  mounted(){
+    // this.$http.get('/api/v1/json').then((data)=>{
+    //   console.log(data)
+    // })
+  },
+  methods: {
+    ...mapActions(['Login']),
+    async handLogin() {
+      this.loading = true;
+      const res = await loginByUsername(1313, 165262302);
+      this.Login(res.data);
+      this.$router.push({ path: '/login' });
+    }
+  }
+}
+</script>
+
+<style lang="less">
+.vux-demo {
+  text-align: center;
+}
+.logo {
+  width: 100px;
+  height: 100px
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,43 @@
 import Vue from 'vue'
+import store from '../store/index'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloFromVux'
 
 Vue.use(Router)
 
-export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+const home = r => require.ensure([], () => r(require('@/page/Home')), 'home')
+const login = r => require.ensure([], () => r(require('@/page/Login')), 'login')
+
+const router = new Router({
+  mode: 'history',
+  routes: [{
+    path: '/',
+    name: 'home',
+    component: home
+  }, {
+    path: '/login',
+    name: 'login',
+    component: login
+  }]
+});
+
+// 注册全局钩子用来拦截导航
+router.beforeEach((to, from, next) => {
+  // 获取store里面的token
+  let token = store.state.token;
+  // 判断要去的路由有没有requiresAuth
+  if (to.meta.requiresAuth) {
+    if (token) {
+      next();
+    } else {
+      next({
+        path: '/login',
+        query: {
+          redirect: to.fullPath
+        } // 将刚刚要去的路由path（却无权限）作为参数，方便登录成功后直接跳转到该路由
+      });
     }
-  ]
-})
+  } else {
+    next(); // 如果无需token,那么随它去吧
+  }
+});
+export default router;
